3ed312470dcdc10c47d986e1c6b5cbc019fe12a1
VitorAntonioMezzalira <62727984+VitorAntonioMezzalira@users.noreply.github.com>
2020-11-13 17:53:26 -0300

    API for characters ok

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,28 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './style.css';
 import Header from '../../components/header/index'
 import Footer from '../../components/footer/index'
 import Aside from '../../components/aside/index'
 
 function HomePage() {
-    return (
-            <div className="interface">
-                <Header />
-                <main className="main">
-                    <div className="characters">
 
-                        <div className="character">
-                            <a href="../hero_page/index.html">
-                                <img src="../images/abaddon.png" alt="Abaddon" />
-                                <div className="character-name" >Abaddon</div>
-                            </a>
-                        </div>
-        
-                    </div>
-                </main>
-                <Footer />
-                <Aside />
-            </div>
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data } = await axios.get('http://localhost:5000/api/characters')
+            setCharacters(data)
+            console.log(data)
+        }
+        fetchData();
+        return () => {
+            //
+        }
+    }, [])
+
+    return (
+        <div className="interface">
+            <Header />
+            <main className="main">
+                <ul className="characters">
+                    {
+                        characters.map(character => {
+                            return (
+                                <li key={character.id} className="character">
+                                    <a href="../hero_page/index.html">
+                                        <img src={ '../images/' + character.image } alt={ character.name } />
+                                        <div className="character-name" >{ character.name }</div>
+                                    </a>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </main>
+            <Footer />
+            <Aside />
+        </div>
     )
 }
 
